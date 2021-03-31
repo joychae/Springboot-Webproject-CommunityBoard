706e95eed1b4aa497ecef2ec9c4a64911913c9f6
joychae <sooie35@gmail.com>
2021-03-31 15:46:44 +0900

    로그아웃 기능 구현

--- src/main/java/com/webproject/community/security/WebSecurityConfig.java
+++ src/main/java/com/webproject/community/security/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
+                .logoutUrl("/user/logout")
                 .permitAll();
 
     }
